26bc75ada97a525593f91dc7e201c2be4bf1299f
Sam <43018132+patator62180@users.noreply.github.com>
2025-01-05 00:50:08 -0500

    instance translation and rotation

--- Main.cpp
+++ Main.cpp
@@ -7,6 +7,32 @@
 #include "bvh.h"
 #include "quad.h"
 
+void simple_scene() {
+    hittable_list world;
+
+    auto red = make_shared<lambertian>(color(.65, .05, .05));
+    auto white = make_shared<lambertian>(color(.73, .73, .73));
+
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5, red));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100, white));
+
+    camera cam;
+
+    cam.aspect_ratio      = 1.0;
+    cam.image_width       = 800;
+    cam.samples_per_pixel = 1000;
+    cam.max_depth         = 50;
+    cam.background        = color(0.5,0.5,0.5);
+
+    cam.vfov     = 40;
+    cam.lookfrom = point3(0, 0, 1);
+    cam.lookat   = point3(0, 0, 0);
+    cam.vup      = vec3(0,1,0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(world);
+}
 
 void bouncing_spheres() {
     hittable_list world;
@@ -211,6 +237,7 @@ void simple_light() {
 
     cam.render(world);
 }
+
 void cornell_box() {
     hittable_list world;
 
@@ -226,44 +253,27 @@ void cornell_box() {
     world.add(make_shared<quad>(point3(555,555,555), vec3(-555,0,0), vec3(0,0,-555), white));
     world.add(make_shared<quad>(point3(0,0,555), vec3(555,0,0), vec3(0,555,0), white));
 
+    shared_ptr<hittable> box1 = box(point3(0,0,0), point3(165,330,165), white);
+    box1 = make_shared<rotate_y>(box1, 15);
+    box1 = make_shared<translate>(box1, vec3(265,0,295));
+    world.add(box1);
+
+    shared_ptr<hittable> box2 = box(point3(0,0,0), point3(165,165,165), white);
+    box2 = make_shared<rotate_y>(box2, -18);
+    box2 = make_shared<translate>(box2, vec3(130,0,65));
+    world.add(box2);
+
     camera cam;
 
     cam.aspect_ratio      = 1.0;
-    cam.image_width       = 400;
-    cam.samples_per_pixel = 1000;
-    cam.max_depth         = 100;
+    cam.image_width       = 200;
+    cam.samples_per_pixel = 2000;
+    cam.max_depth         = 50;
     cam.background        = color();
 
     cam.vfov     = 40;
     cam.lookfrom = point3(278, 278, -800);
     cam.lookat   = point3(278, 278, 0);
-    cam.vup      = vec3(0,1,0);
-
-    cam.defocus_angle = 0;
-
-    cam.render(world);
-}
-
-void simple_scene() {
-    hittable_list world;
-
-    auto red = make_shared<lambertian>(color(.65, .05, .05));
-    auto white = make_shared<lambertian>(color(.73, .73, .73));
-
-    world.add(make_shared<sphere>(point3(0,0,-1), 0.5, red));
-    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100, white));
-
-    camera cam;
-
-    cam.aspect_ratio      = 1.0;
-    cam.image_width       = 400;
-    cam.samples_per_pixel = 200;
-    cam.max_depth         = 50;
-    cam.background        = color(0.5,0.5,0.5);
-
-    cam.vfov     = 40;
-    cam.lookfrom = point3(0, 0, 1);
-    cam.lookat   = point3(0, 0, 0);
     cam.vup      = vec3(0,1,0);
 
     cam.defocus_angle = 0;
